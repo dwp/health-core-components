f4e4359b55865b50d270fb286d7798e5c9e0e692
colpas <62711290+colpas@users.noreply.github.com>
2020-11-13 17:43:18 +0000

    tasklist

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const router = express.Router()
-
-// Add your routes here - above the module.exports line
-
-module.exports = router
-
-
+ 
 // statement Navigation
-
+ 
 router.post("/wca_v1/TakingStatementsentry-post", function(req, res, next){
-
-  if(req.body['comment'] === 'add-statement'){
-    res.redirect('/wca_v1/condition_history_saved');
-  } else {
-    res.redirect('/wca_v1/TakingStatementsentry');
-  }
-  next();
+ 
+ if(req.body['comment'] === 'add-statement'){
+ res.redirect('/wca_v1/condition_history_saved');
+ } else {
+ res.redirect('/wca_v1/TakingStatementsentry');
+ }
+ next();
 });
+ 
+// task list
+ 
+router.get('/wca/task-list', function (req, res) {
+ res.render('corecomponents/Research/3Navigation/WCA-tasklist/TaskList.html', { session: req.session.data })
+})
+ 
+router.get('/wca/condition-history', function (req, res) {
+ res.render('corecomponents/Research/3Navigation/WCA-tasklist/Conditionhistory.html', { session: req.session.data });
+});
+ 
+router.post('/wca/condition-history/save-and-continue', function (req, res) {
+ if (req.body['condition-history']) {
+ req.session.data['condition-history'] = req.body['condition-history']
+ }
+ res.redirect('/wca/task-list');
+});
+ 
+module.exports = router
